1a808670153b5e0df732ff0f78128004d83bc8d1
Robson <William>
2019-07-23 17:07:55 -0300

    Refatoração na validação de inclusão de novos servidores
    Refatoração na validação de inclusão de novos servidores, comitando apenas os arquivos modificados.

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,15 +1,30 @@
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import '@webcomponents/webcomponentsjs';
 
+import '@material/mwc-button';
+import '@material/mwc-checkbox';
+import '@material/mwc-icon';
+import '@material/mwc-icon-button';
+import '@material/mwc-linear-progress';
+import '@material/mwc-ripple';
+import '@material/mwc-switch';
+
+import './monitor-add-server-dialog';
 import './monitor-app';
 import './monitor-app-bar';
 import './monitor-body';
+import './monitor-button';
+import './monitor-checkbox';
 import './monitor-drawer';
-import './monitor-add-server-dialog';
+import './monitor-linear-progress';
+import './monitor-main';
+import './monitor-radio';
+import './monitor-ripple';
+import './monitor-server-view';
 import './monitor-text-input';
 import './monitor-user-list';
-import './monitor-ripple';
-import './monitor-linear-progress';
+import "./monitor-warning";
 
 import { TdsLanguageClient } from '@totvs/tds-languageclient';
 
@@ -29,12 +44,14 @@ else {
 			{
 				name: "Production",
 				address: "LOCALHOST",
-				port: 5555
+				port: 5555,
+				build: '7.00.170117A'
 			},
 			{
 				name: "LOBO-GUARA",
 				address: "192.168.168.1",
-				port: 6000
+				port: 6000,
+				build: '7.00.170117A'
 			}
 		]
 	}
--- src/main/ts/monitor-text-input.ts
+++ src/main/ts/monitor-text-input.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, customElement, CSSResult, property } from 'lit-element';
+import { CSSResult, customElement, html, LitElement, property } from 'lit-element';
 import { style } from '../css/monitor-text-input.css';
 
 @customElement('monitor-text-input')
@@ -11,10 +11,22 @@ export class MonitorTextInput extends LitElement {
 	value: string = '';
 
 	@property({ type: String, reflect: true, attribute: true })
-	label: string = '';
+	label: string = null;
 
 	@property({ type: Boolean, reflect: true, attribute: true })
 	disabled: boolean = false;
+
+	@property({ type: Boolean, reflect: true, attribute: true })
+	outlined: boolean = false;
+
+	@property({ type: String, reflect: true, attribute: true })
+	icon: string = null;
+
+	@property({ type: Number, reflect: true, attribute: true })
+	min: number = null;
+
+	@property({ type: Number, reflect: true, attribute: true })
+	max: number = null;
 
 	createRenderRoot() {
 		return this.attachShadow({ mode: 'open', delegatesFocus: true });
@@ -26,7 +38,7 @@ export class MonitorTextInput extends LitElement {
 		//this.setAttribute('tabindex', '0');
 
 		this.addEventListener('focus', (event: Event) => {
-			this.renderRoot.querySelector('input').focus();
+			this.renderRoot.querySelector<HTMLInputElement | HTMLTextAreaElement>('input, textarea').focus();
 		})
 
 	}
@@ -36,16 +48,45 @@ export class MonitorTextInput extends LitElement {
 	}
 
 	render() {
+		// let min = html`${((this.type === 'number') && (this.min !== null)) ? 'min="${this.min}"' : ''}`,
+		// max = html`${((this.type === 'number') && (this.max !== null)) ? 'max="${this.max}"' : ''}`;
+
+		//max = ((this.type === 'number') && (this.max !== null)) ? html`min="${this.max}"` : '';
+
+		let min = (this.type === 'number') ? this.min : null,
+			max = (this.type === 'number') ? this.max : null;
+
+		//				${min !== null ? html`min="${min}"` : ''}
+		//${max !== null ? html`max="${max}"` : ''}
+
 		return html`
-			<div class="${this.value === '' ? ' is-empty' : ''}">
+			<div class="${this.value === '' ? 'is-empty' : ''}">
+				${this.icon ? html`<mwc-icon-button icon="${this.icon}"></mwc-icon-button>` : ''}
+				${this.type !== 'textarea' ? html`
 				<input type="${this.type}" .value="${this.value}" ?disabled=${this.disabled} @change="${this.onInputChanged}" @keydown="${this.onInputKeyDown}"
+				 min=${min} max=${max} tabindex="0" />
+				` : html`
+				<textarea .value="${this.value}" ?disabled=${this.disabled} @change="${this.onInputChanged}" @keydown="${this.onInputKeyDown}"
 				 tabindex="0" />
-				<label>${this.label}</label>
-				<hr />
-			</div>
+				`}
+							<label>${this.label}</label>
+							${this.outlined ? '' : html`<hr />`}
+						</div>
 		`;
 	}
 
+
+	showRedLine(fieldContent: string, fieldType: string) {
+		var oldLabel = this.label;
+		this.renderRoot.querySelector('div').classList.add('invalid');
+		this.label = fieldContent;
+		this.type = fieldType;
+
+		setTimeout(() => {
+			this.label = oldLabel;
+
+		}, 3000);
+	}
 
 	onInputChanged(event: Event) {
 		this.value = (event.target as HTMLInputElement).value;
@@ -64,6 +105,8 @@ export class MonitorTextInput extends LitElement {
 
 				break;
 		}
+
+		this.renderRoot.querySelector('div').classList.remove('invalid');
 	}
 
 }
