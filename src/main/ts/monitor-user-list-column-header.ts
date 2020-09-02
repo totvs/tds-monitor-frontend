d8fd49c7b89d9764aab5e8daa28840768d1e8e93
lwtnb-wrk <leo.watanabe@totvs.com.br>
2020-09-02 12:25:12 -0300

    implementacao ajuste de tamanho de colunas

--- src/main/ts/monitor-user-list-column-header.ts
+++ src/main/ts/monitor-user-list-column-header.ts
@@ -1,6 +1,8 @@
 //import { MonitorUser } from '@totvs/tds-languageclient';
 import { CSSResult, customElement, html, LitElement, property } from 'lit-element';
 import { style } from '../css/monitor-user-list-column-header.css';
+import { columnConfig } from './monitor-columns';
+import { i18n } from './util/i18n';
 
 export enum SortOrder {
 	Undefined = 'undefined',
@@ -10,6 +12,12 @@ export enum SortOrder {
 
 @customElement('monitor-user-list-column-header')
 export class MonitorUserListColumnHeader extends LitElement {
+
+	initialX: number = null;
+	initialWidth: number = null;
+
+	@property({ type: String, reflect: true, attribute: true })
+	id: string = '';
 
 	@property({ type: String, reflect: true, attribute: true })
 	order: SortOrder = SortOrder.Undefined;
@@ -22,8 +30,6 @@ export class MonitorUserListColumnHeader extends LitElement {
 
 	constructor() {
 		super();
-
-		this.addEventListener('click', this.onClick);
 	}
 
 	static get styles(): CSSResult {
@@ -31,14 +37,16 @@ export class MonitorUserListColumnHeader extends LitElement {
 	}
 
 	render() {
-		//const icon = this.order == SortOrder.Descending ? 'arrow_drop_up' : 'arrow_drop_down';
 		let columnWidth = this.width;
-		let captionWidth = columnWidth - 24; // subtract arrow icon width
+		let captionWidth = columnWidth - 24 - 5; // subtract arrow icon width
 		return html`
-			<th style="width:${columnWidth}px;">
-				<label title="${this.caption}" style="width:${captionWidth}px;">${this.caption}</label>
+			<div class="clickable" style="width:${columnWidth}px;" @click=${this.onClick}>
+				<div class="label" style="width:${captionWidth}px;">
+					<label title="${this.caption}">${this.caption}</label>
+				</div>
 				<i>arrow_downward</i>
-			</th>
+			</div>
+			<div class="resize" @mousedown="${this.onResizeStart}"></div>
 		`;
 	}
 
@@ -50,6 +58,59 @@ export class MonitorUserListColumnHeader extends LitElement {
 
 
 		this.dispatchEvent(new HeaderClickEvent(this.id as ColumnKey, this.order));
+	}
+
+	onResizeStart(event: MouseEvent) {
+		const moveListener = (event: MouseEvent) => this.onResizeMove(event);
+		const endListener = (event: MouseEvent) => {
+			document.removeEventListener('mousemove', moveListener);
+			document.removeEventListener('mouseup', endListener);
+			document.removeEventListener('mouseout', endListener);
+
+			return this.onResizeEnd(event);
+		};
+
+		document.addEventListener('mousemove', moveListener);
+		document.addEventListener('mouseup', endListener);
+		document.addEventListener('mouseout', endListener);
+
+		this.initialX = event.clientX;
+		this.initialWidth = this.clientWidth;
+
+		document.body.classList.add('resizing-ns');
+
+		return false;
+	}
+
+	onResizeMove(event: MouseEvent) {
+		let newWidth = this.initialWidth + (event.clientX - this.initialX) - 10;
+		this.width = newWidth;
+	}
+
+	onResizeEnd(event: MouseEvent) {
+		document.body.classList.remove('resizing-ns');
+
+		const app = document.querySelector('monitor-app');
+		let columns = columnConfig().map((column) => {
+			return {
+				id: column.id,
+				visible: column.visible,
+				align: column.align,
+				width: (column.id === this.id) ? this.width : column.width
+			}
+		});
+
+		app.config = Object.assign<MonitorSettingsConfig, MonitorSettingsConfig>(app.config, {
+			columns
+		});
+
+		app.dispatchEvent(new CustomEvent<string>('settings-update', {
+			detail: i18n.localize("UPDATE_SETTINGS", "Update settings."),
+			bubbles: true,
+			composed: true
+		}));
+
+		return false;
 	}
 
 }
