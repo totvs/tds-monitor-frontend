8594ed08133f8dcb49da53a964ea97818ab6ba9d
Alan Cândido <brodao@gmail.com>
2021-07-20 08:58:26 -0300

    Adicionado opção de idioma russo, idioma padrão o inglês e atualização de binários.

--- src/main/ts/monitor-about-dialog.ts
+++ src/main/ts/monitor-about-dialog.ts
@@ -1,24 +1,26 @@
-import { CSSResult, customElement, html } from 'lit-element';
-import { style } from '../css/monitor-about-dialog.css';
-import { MonitorDialog } from './monitor-dialog';
-import { i18n } from './util/i18n';
+import { CSSResult, customElement, html } from "lit-element";
+import { style } from "../css/monitor-about-dialog.css";
+import { MonitorDialog } from "./monitor-dialog";
+import { i18n } from "./util/i18n";
 
-@customElement('monitor-about-dialog')
+@customElement("monitor-about-dialog")
 export class MonitorAboutDialog extends MonitorDialog {
-
 	constructor() {
 		super({
 			escClose: true,
 			buttons: [
 				{
 					text: i18n.close(),
-					click: (event) => this.onCloseButtonClicked(event)
-				}
-			]
+					click: (event) => this.onCloseButtonClicked(event),
+				},
+			],
 		});
 
-		this.title = i18n.localize("ABOUT_TOTVS_MONITOR", "About TOTVS Monitor");
-		this.progress = 'none';
+		this.title = i18n.localize(
+			"ABOUT_TOTVS_MONITOR",
+			"About TOTVS Monitor"
+		);
+		this.progress = "none";
 	}
 
 	static get styles(): CSSResult {
@@ -26,21 +28,25 @@ export class MonitorAboutDialog extends MonitorDialog {
 	}
 
 	get body() {
-		const app = document.querySelector('monitor-app');
+		const app = document.querySelector("monitor-app");
 
 		return html`
-			<img>
+			<img />
 			<h1>${i18n.totvsMonitor()}</h1>
 			<h4>v${app.version}</h4>
-			${Object.keys(app.dependencies).map((key: keyof Versions) => html`
-				<h4>${key}: ${app.dependencies[key]}</h4>
-			`)}
+			${Object.keys(app.dependencies).map(
+				(key: keyof Versions) =>
+					html`
+						<h4>
+							${key.replace("@totvs", "@")}:
+							${app.dependencies[key]}
+						</h4>
+					`
+			)}
 		`;
 	}
 
 	onCloseButtonClicked(event: Event) {
 		this.close();
 	}
-
 }
-
--- src/main/ts/monitor-settings-dialog.ts
+++ src/main/ts/monitor-settings-dialog.ts
@@ -16,19 +16,19 @@ export class MonitorSettingsDialog extends MonitorDialog {
 			buttons: [
 				{
 					text: i18n.confirm(),
-					click: (event) => this.onOkButtonClicked(event)
+					click: (event) => this.onOkButtonClicked(event),
 				},
 				{
 					text: i18n.cancel(),
-					click: (event) => this.onCancelButtonClicked(event)
-				}
-			]
+					click: (event) => this.onCancelButtonClicked(event),
+				},
+			],
 		});
 
 		this.title = i18n.localize("CONFIGURATIONS", "Configurations");
-		this.progress = 'none';
+		this.progress = "none";
 
-		const app = document.querySelector('monitor-app');
+		const app = document.querySelector("monitor-app");
 		this.currentLanguage = app.config.language;
 	}
 
@@ -37,49 +37,109 @@ export class MonitorSettingsDialog extends MonitorDialog {
 	}
 
 	render() {
-		return html`
-			${super.render()}
-		`;
+		return html` ${super.render()} `;
 	}
 
 	get body() {
-		const app = document.querySelector('monitor-app'),
+		const app = document.querySelector("monitor-app"),
 			{
 				language,
 				updateInterval,
 				alwaysOnTop,
 				generateUpdateLog,
-				generateExecutionLog
+				generateExecutionLog,
 			} = app.config;
 
-		return html`
-			<section>
-				<label for="language">${i18n.localize("LANGUAGE", "Language")}</label>
+		return html` <section>
+				<label for="language"
+					>${i18n.localize("LANGUAGE", "Language")}</label
+				>
 				<select id="language">
-					<option value="portuguese" ?selected="${language === 'portuguese'}">${i18n.localize("PORTUGUESE", "Portuguese")}</option>
-					<option value="english" ?selected="${language === 'english'}">${i18n.localize("ENGLISH", "English")}</option>
-					<option value="spanish" ?selected="${language === 'spanish'}">${i18n.localize("SPANISH", "Spanish")}</option>
+					<option
+						value="portuguese"
+						?selected="${language === "portuguese"}"
+					>
+						${i18n.localize("PORTUGUESE", "Portuguese")}
+					</option>
+					<option
+						value="english"
+						?selected="${language === "english"}"
+					>
+						${i18n.localize("ENGLISH", "English")}
+					</option>
+					<option
+						value="spanish"
+						?selected="${language === "spanish"}"
+					>
+						${i18n.localize("SPANISH", "Spanish")}
+					</option>
+					<option
+						value="russian"
+						?selected="${language === "russian"}"
+					>
+						${i18n.localize("RUSSIAN", "Russian")}
+					</option>
 				</select>
 			</section>
 
 			<section>
-				<label for="updateInterval">${i18n.localize("INFORMATION_UPDATE_INTERVAL", "Information update interval")}</label>
-				<monitor-text-input id="updateInterval" tabindex="1" type="number" min="0" max="3600" small value="${updateInterval}"></monitor-text-input>
+				<label for="updateInterval"
+					>${i18n.localize(
+						"INFORMATION_UPDATE_INTERVAL",
+						"Information update interval"
+					)}</label
+				>
+				<monitor-text-input
+					id="updateInterval"
+					tabindex="1"
+					type="number"
+					min="0"
+					max="3600"
+					small
+					value="${updateInterval}"
+				></monitor-text-input>
 			</section>
 
 			<!--[TODO: Falta implementar]-->
 			<strike>
 				<section>
-				<label for="alwaysOnTop">${i18n.localize("KEEP_ON_TOP", "Keep this on top")}</label>
-				<mwc-switch disabled id="alwaysOnTop" ?checked=${alwaysOnTop}></mwc-switch>
+					<label for="alwaysOnTop"
+						>${i18n.localize(
+							"KEEP_ON_TOP",
+							"Keep this on top"
+						)}</label
+					>
+					<mwc-switch
+						disabled
+						id="alwaysOnTop"
+						?checked=${alwaysOnTop}
+					></mwc-switch>
 				</section>
 				<section>
-				<label for="generateUpdateLog">${i18n.localize("GENERATE_LOG", "Generate log files on every update")}</label>
-				<mwc-switch disabled id="generateUpdateLog" ?checked=${generateUpdateLog}></mwc-switch>
+					<label for="generateUpdateLog"
+						>${i18n.localize(
+							"GENERATE_LOG",
+							"Generate log files on every update"
+						)}</label
+					>
+					<mwc-switch
+						disabled
+						id="generateUpdateLog"
+						?checked=${generateUpdateLog}
+					></mwc-switch>
 				</section>
 				<section>
-				<label for="generateExecutionLog">${i18n.localize("GENERATE_EXEC_LOG", "Generate execution log")}</label>
-				<mwc-switch disabled id="generateExecutionLog" ?checked=${generateExecutionLog}></mwc-switch>
+					<label for="generateExecutionLog"
+						>${i18n.localize(
+							"GENERATE_EXEC_LOG",
+							"Generate execution log"
+						)}</label
+					>
+					<mwc-switch
+						disabled
+						id="generateExecutionLog"
+						?checked=${generateExecutionLog}
+					></mwc-switch>
 				</section>
 			</strike>`;
 	}
@@ -87,26 +147,49 @@ export class MonitorSettingsDialog extends MonitorDialog {
 	onOkButtonClicked(event: Event) {
 		//console.log('onOkButtonClicked: %s', this.renderRoot.querySelector<MonitorTextInput>('#updateInterval').value);
 
-		const app = document.querySelector('monitor-app');
+		const app = document.querySelector("monitor-app");
 
-		app.config = Object.assign<MonitorSettingsConfig, MonitorSettingsConfig>(app.config, {
-			updateInterval: parseInt(this.renderRoot.querySelector<MonitorTextInput>('#updateInterval').value, 10),
-			language: this.renderRoot.querySelector<HTMLSelectElement>('#language').value as any,
-			alwaysOnTop: this.renderRoot.querySelector<MonitorCheckbox>('#alwaysOnTop').checked,
-			generateUpdateLog: this.renderRoot.querySelector<MonitorCheckbox>('#generateUpdateLog').checked,
-			generateExecutionLog: this.renderRoot.querySelector<MonitorCheckbox>('#generateExecutionLog').checked
+		app.config = Object.assign<
+			MonitorSettingsConfig,
+			MonitorSettingsConfig
+		>(app.config, {
+			updateInterval: parseInt(
+				this.renderRoot.querySelector<MonitorTextInput>(
+					"#updateInterval"
+				).value,
+				10
+			),
+			language: this.renderRoot.querySelector<HTMLSelectElement>(
+				"#language"
+			).value as any,
+			alwaysOnTop:
+				this.renderRoot.querySelector<MonitorCheckbox>("#alwaysOnTop")
+					.checked,
+			generateUpdateLog:
+				this.renderRoot.querySelector<MonitorCheckbox>(
+					"#generateUpdateLog"
+				).checked,
+			generateExecutionLog:
+				this.renderRoot.querySelector<MonitorCheckbox>(
+					"#generateExecutionLog"
+				).checked,
 		});
 
-		if (this.currentLanguage && app.config.language !== this.currentLanguage) {
+		if (
+			this.currentLanguage &&
+			app.config.language !== this.currentLanguage
+		) {
 			const dialog = new MonitorLanguageChangeDialog();
 			dialog.show();
 		}
 
-		app.dispatchEvent(new CustomEvent<string>('settings-update', {
+		app.dispatchEvent(
+			new CustomEvent<string>("settings-update", {
 				detail: i18n.localize("UPDATE_SETTINGS", "Update settings."),
 				bubbles: true,
-			composed: true
-		}));
+				composed: true,
+			})
+		);
 
 		this.close();
 	}
